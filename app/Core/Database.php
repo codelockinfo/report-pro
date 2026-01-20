faadbcd76f1cb7caecf1370f37d49fe804d1421d
Codelock <codelockinfo@gmail.com>
2026-01-20 15:18:08 +0530

    Fix PDO constants in Database class

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -22,9 +22,9 @@ class Database
             );
 
             $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_EMULATE_PREPARES => false,
             ];
 
             $this->pdo = new \PDO($dsn, $dbConfig['user'], $dbConfig['password'], $options);
